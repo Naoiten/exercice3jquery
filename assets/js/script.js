9be81e70a1d84184a20b22031bb64f33b6896f96
Antoine <antoine.hotiin32@laposte.net>
2019-07-19 16:55:38 +0200

    exercice finis

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+                $('#moins').hover(function(){
+                    $("#firstText").css('font-size','10px');
+                });
+
+                $('#plus').hover(function(){
+                    $("#firstText").css('font-size','20px');
+                });
+                });
